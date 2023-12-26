f7d569d03c08e56139ae7c236a589ab1d6449737
Milan <milanvaddoriya182@gmail.com>
2023-12-26 18:54:41 +0530

    Done changing for testing

--- app.py
+++ app.py
@@ -120,5 +120,5 @@ def get_data():
 def index():
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
