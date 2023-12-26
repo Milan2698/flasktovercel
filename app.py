ec67fce86c726179399151e6bffa923cd5f2e9de
Milan <milanvaddoriya182@gmail.com>
2023-12-26 18:44:14 +0530

    Done changing for testing

--- app.py
+++ app.py
@@ -18,55 +18,65 @@ def verify_password(username, password):
 def firstColumn(search):
     newSearch = '+'.join(search.split())
     url = f'https://www.ebay.com/sch/i.html?_from=R40&_nkw={newSearch}&LH_BIN=1&LH_ItemCondition=3000&_sop=15'
+    print(url)
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
-    productPriceTag = soup.find('ul', class_='srp-results srp-list clearfix').find('span', class_='s-item__price')
-    if productPriceTag is not None:
-        productPrice = productPriceTag.text
-        heading = soup.find_all('div', class_='s-item__title')[1].find('span', role='heading').text
-        if search.split()[-1] not in heading:
-            productPrice = None
-    else:
-        productPrice = None
+    total_result = int(soup.find('h1', class_='srp-controls__count-heading').find('span').text)
+    productPrice = None
+    if total_result != 0:
+        productPriceTag = soup.find('span', class_='s-item__price')
+        if productPriceTag is not None:
+            productPrice = productPriceTag.text
+            heading = soup.find_all('div', class_='s-item__title')[1].find('span', role='heading').text
+            if search.split()[-1] not in heading:
+                productPrice = None
+
         
     return productPrice
 
 def secondColumn(search):
     newSearch = '+'.join(search.split())
     url = f'https://www.ebay.com/sch/i.html?_from=R40&_nkw={newSearch}&LH_BIN=1&LH_Sold=1&LH_ItemCondition=3000&_sop=16'
+    print(url)
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
-    productPriceTag = soup.find('ul', class_='srp-results srp-list clearfix').find('span', class_='s-item__price')
-    if productPriceTag is not None:
-        productPrice = productPriceTag.text
-        heading = soup.find_all('div', class_='s-item__title')[1].find('span', role='heading').text
-        if search.split()[-1] not in heading:
-            productPrice = None
-    else:
-        productPrice = None
+    total_result = int(soup.find('h1', class_='srp-controls__count-heading').find('span').text)
+    productPrice = None
+    if total_result != 0:
+        productPriceTag = soup.find('span', class_='s-item__price')
+        if productPriceTag is not None:
+            productPrice = productPriceTag.text
+            heading = soup.find_all('div', class_='s-item__title')[1].find('span', role='heading').text
+            if search.split()[-1] not in heading:
+                productPrice = None
+
     return productPrice
 
 def thirdColumn(search):
     newSearch = '+'.join(search.split())
     url = f'https://www.ebay.com/sch/i.html?_from=R40&_nkw={newSearch}&LH_ItemCondition=1000&LH_BIN=1&_sop=15'
+    print(url)
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
-    productPriceTag = soup.find('ul', class_='srp-results srp-list clearfix').find('span', class_='s-item__price')
-    if productPriceTag is not None:
-        productPrice = productPriceTag.text
-        heading = soup.find_all('div', class_='s-item__title')[1].find('span', role='heading').text
-        if search.split()[-1] not in heading:
-            productPrice = None
-    else:
-        productPrice = None
+    
+    total_result = int(soup.find('h1', class_='srp-controls__count-heading').find('span').text)
+    productPrice = None
+    if total_result != 0:
+        productPriceTag = soup.find('span', class_='s-item__price')
+        if productPriceTag is not None:
+            productPrice = productPriceTag.text
+            heading = soup.find_all('div', class_='s-item__title')[1].find('span', role='heading').text
+            if search.split()[-1] not in heading:
+                productPrice = None
+
     return productPrice
 
 
 @app.route('/api/get_data', methods=['GET'])
 def get_data():
     modelNumber = request.args.get('modelNumber')
-
-    partSelectURL = 'https://www.partselect.com/Models/{}/'.format(modelNumber)
+    print(modelNumber)
+    partSelectURL = 'https://www.partselect.com/Models/{}/Parts/'.format(modelNumber)
     response = requests.get(partSelectURL)
     soup = BeautifulSoup(response.content, 'html.parser')
     appliance = soup.find('h1').text.split(modelNumber)[-1].split()[0]
@@ -88,9 +98,10 @@ def get_data():
             manufacturerNum = modelEle.get_text(strip=True).split(':')[-1].strip()
             search = appliance + ' ' + manufacturerNum
             searches.append(search)
+            print(len(searches))
 
     data = []
-    for search in searches:
+    for search in searches[:10]:
         di = {}
         first = firstColumn(search)
         second = secondColumn(search)
@@ -108,3 +119,6 @@ def get_data():
 @auth.login_required
 def index():
     return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
